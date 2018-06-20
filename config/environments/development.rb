2e1539c000ee7ff0aeea470929917b3bf1b68782
kickbuttowski59 <pazpaz22@yahoo.com>
2018-06-20 23:01:26 +0000

    looking why edit password raises an error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,5 +58,19 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://todo-kickbuttowski59.c9users.io/'}
+ 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        authentication: "plain",
+        enable_starttls_auto: true,
+        user_name: "pazpaz25@gmail.com",
+        password: "zvzcfbsrsfgjovop",
+        domain: 'gmail.com'
+  
+    }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+   config.action_mailer.default_url_options = { host: 'https://todo-4more1day-bykickbuttowski.herokuapp.com'}
+ 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        authentication: "plain",
+        enable_starttls_auto: true,
+        user_name: "pazpaz25@gmail.com",
+        password: "zvzcfbsrsfgjovop",
+        domain: 'gmail.com'
+  
+    }
 end
