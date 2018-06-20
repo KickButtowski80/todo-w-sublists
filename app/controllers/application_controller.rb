f259b3c3e72d44419a1205deef7b151b2811782e
kickbuttowski59 <pazpaz22@yahoo.com>
2018-06-20 05:07:05 +0000

    by clicking on navbar logo , all the list will be shown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     
      before_action :configure_permitted_parameters, if: :devise_controller?
+     before_action :set_todo_list
      
      protected
 
@@ -8,4 +9,11 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
         # devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       end
+      
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_todo_list
+      @todo_list = TodoList.find(params[:id])      
+    end
+    
 end
